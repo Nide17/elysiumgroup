e6d58835f664e11c1b99e161a9ad43246690c8ae
Nide17 <nidedrogba@gmail.com>
2021-07-10 10:57:26 +0200

    home carousel

--- client/src/components/main/carousel/SlideCarousel.js
+++ client/src/components/main/carousel/SlideCarousel.js
@@ -34,7 +34,8 @@ const SlideCarousel = () => {
                                 <img className="d-block w-100 h-100" src={architecture1} alt="First slide" />
                                 <div className="carousel-caption d-none d-md-block">
                                     <h4>Architecture & Design</h4>
-                                    <p><a href="#reach-us">Beautiful designs from professionals</a></p>
+                                    <p><a href="#details">Beautiful designs from professionals</a></p>
+                                    <a href="/contact" class="btn mt-4 contact-button">CONTACT US</a>
                                 </div>
                             </div>
 
@@ -42,7 +43,8 @@ const SlideCarousel = () => {
                                 <img className="d-block w-100 h-100" src={construction} alt="Second slide" />
                                 <div className="carousel-caption d-none d-md-block">
                                     <h4>Building & Construction</h4>
-                                    <p><a href="#reach-us">Our engineers deliver excellent construction services</a></p>
+                                    <p><a href="#details">Our engineers deliver excellent construction services</a></p>
+                                    <a href="/contact" class="btn mt-4 contact-button">CONTACT US</a>
                                 </div>
                             </div>
 
@@ -50,8 +52,8 @@ const SlideCarousel = () => {
                                 <img className="d-block w-100 h-100" src={road} alt="Third slide" />
                                 <div className="carousel-caption d-none d-md-block">
                                     <h4>Roads & Railway Activities</h4>
-                                    <p><a href="#reach-us">Need a road or Railway? Contact us.</a></p>
-
+                                    <p><a href="#details">Need a road or Railway? CONTACT US.</a></p>
+                                    <a href="/contact" class="btn mt-4 contact-button">CONTACT US</a>
                                 </div>
                             </div>
 
@@ -59,8 +61,8 @@ const SlideCarousel = () => {
                                 <img className="d-block w-100 h-100" src={site} alt="Fourth slide" />
                                 <div className="carousel-caption d-none d-md-block">
                                     <h4>Site Preparation Activities</h4>
-                                    <p><a href="#reach-us">If you want to prepare your site, the solution is here.</a></p>
-
+                                    <p><a href="#details">If you want to prepare your site, the solution is here.</a></p>
+                                    <a href="/contact" class="btn mt-4 contact-button">CONTACT US</a>
                                 </div>
                             </div>
 
@@ -68,7 +70,8 @@ const SlideCarousel = () => {
                                 <img className="d-block w-100 h-100" src={electrical} alt="Fifth slide" />
                                 <div className="carousel-caption d-none d-md-block">
                                     <h4>Electrical & Power Activities</h4>
-                                    <p><a href="#reach-us">We have excellent services related to the power and electrical engineering</a></p>
+                                    <p><a href="#details">We have excellent services related to the power and electrical engineering</a></p>
+                                    <a href="/contact" class="btn mt-4 contact-button">CONTACT US</a>
                                 </div>
                             </div>
 
@@ -76,8 +79,8 @@ const SlideCarousel = () => {
                                 <img className="d-block w-100 h-100" src={water} alt="Sixth slide" />
                                 <div className="carousel-caption d-none d-md-block">
                                     <h4>Water & Plumbing Activities</h4>
-                                    <p><a href="#reach-us">Award winning plumber and water engineers of us are waiting for you</a></p>
-
+                                    <p><a href="#details">Award winning plumber and water engineers of us are waiting for you</a></p>
+                                    <a href="/contact" class="btn mt-4 contact-button">CONTACT US</a>
                                 </div>
                             </div>
 
@@ -85,8 +88,8 @@ const SlideCarousel = () => {
                                 <img className="d-block w-100 h-100" src={accountants} alt="Seventh slide" />
                                 <div className="carousel-caption d-none d-md-block">
                                     <h4>Accounting & Consultancy</h4>
-                                    <p><a href="#reach-us">Why can't you work with our accountants and tax advisers?</a></p>
-
+                                    <p><a href="#details">Why can't you work with our accountants and tax advisers?</a></p>
+                                    <a href="/contact" class="btn mt-4 contact-button">CONTACT US</a>
                                 </div>
                             </div>
 
@@ -94,8 +97,8 @@ const SlideCarousel = () => {
                                 <img className="d-block w-100 h-100" src={web4} alt="Seventh slide" />
                                 <div className="carousel-caption d-none d-md-block">
                                     <h4>Web Development Services</h4>
-                                    <p><a href="#reach-us">Want to put your business online? Welcome, we want to help you!</a></p>
-
+                                    <p><a href="#details">Want to put your business online? Welcome, we want to help you!</a></p>
+                                    <a href="/contact" class="btn mt-4 contact-button">CONTACT US</a>
                                 </div>
                             </div>
 
